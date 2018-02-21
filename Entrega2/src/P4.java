36b7443564028bdb6d53eff3164f5ae5997dd8b9
Santiago Gómez <santii810@hotmail.com>
2018-02-21 23:41:51 +0100

    Entrega 2 Apartado 1b

---
+++ Entrega2/src/P4.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class P4 {
+	final static int NUMBER_OF_THREADS = 32;
+
+	public static void main(String[] args) {
+		ArrayList<MiHilo> threadList = new ArrayList<MiHilo>(NUMBER_OF_THREADS);
+		for (int i = 0; i < NUMBER_OF_THREADS; i++) {
+			String nombreHilo = "Thread number " + (i+1);
+			threadList.add(new MiHilo(nombreHilo));
+			new Thread(threadList.get(i)).start();
+		}
+		System.out.println("Program of exercise P4 has terminated");
+	}
+}
+
+class MiHilo extends Thread {
+	public MiHilo(String nombreHilo) {
+		super(nombreHilo);
+	}
+	@Override
+	public void run() {
+		System.out.println("Bye, this was " + currentThread().getName());
+	}
+}
