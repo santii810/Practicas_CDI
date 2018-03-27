a079cee7aa4709333d553aecda5d2002a159e7df
Santiago Gómez <santii810@hotmail.com>
2018-03-27 18:46:05 +0200

    P3 entregada

--- Entrega6/P3/P3_d/ejecutar.bat
+++
@@ -1,8 +0,0 @@
-rem @echo off
-del .\ModoExplicito.dat
-del .\ModoFuerzaBruta.dat
-
-	for /L %%x IN (0,200,10000) DO (
-		java ModoFuerzaBruta 8 %%x >> ModoFuerzaBruta.dat
-		java ModoExplicito 8 %%x >> ModoExplicito.dat
-	)
