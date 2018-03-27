871a8e0a10ac320970df3252b98084bbe5045338
santii810 <santii810@hotmail.com>
2018-03-27 18:20:46 +0200

    Suavizado de graficas

--- Entrega6/P3/P3_d/ejecutar.bat
+++ Entrega6/P3/P3_d/ejecutar.bat
@@ -2,7 +2,7 @@ rem @echo off
 del .\ModoExplicito.dat
 del .\ModoFuerzaBruta.dat
 
-	for /L %%x IN (10,10,3000) DO (
+	for /L %%x IN (0,200,10000) DO (
 		java ModoFuerzaBruta 8 %%x >> ModoFuerzaBruta.dat
 		java ModoExplicito 8 %%x >> ModoExplicito.dat
 	)
